927a74d19779acd8098588942fdc7acd94643a49
Omair-D <odurrani@kent.edu>
2026-02-20 15:10:11 -0500

    Updated homepage message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello from Node.js on Render!");
+  res.send("Hello! This is Omair Durrani’s Web Programming II app – Version 2");
 });
 
 app.listen(PORT, () => {
